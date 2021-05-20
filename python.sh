2874a6659e42ae07de53a7ef68c823dc366f33ae
NightRaven <53287573+Xynnix@users.noreply.github.com>
2021-05-19 23:51:45 -0400

    Create python.sh

---
+++ python.sh
@@ -0,0 +1,21 @@
+# Step 1 – Prerequisite
+sudo apt-get install build-essential checkinstall -y
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
+
+#Step 2 – Download Python 3.8
+cd /opt
+sudo wget https://www.python.org/ftp/python/3.8.7/Python-3.8.7.tgz
+sudo tar xzf Python-3.8.7.tgz
+
+# Step 3 – Compile Python Source
+cd Python-3.8.7
+sudo ./configure --enable-optimizations
+sudo make altinstall
+
+# Step 4 – Check Python Version
+python3.8 -V
+
+# Step 5 – Remove Python
+cd /opt
+sudo rm -f Python-3.8.7.tgz
